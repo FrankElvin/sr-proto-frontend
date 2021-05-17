2cd9ceb9476f2a179e3238f0117863421d1e3865
FrankElvin <zhdanov.noc.sm@gmail.com>
2021-05-18 01:49:31 +0300

    Enhanced loading of all reports

--- src/AllReports.elm
+++ src/AllReports.elm
@@ -30,9 +30,7 @@ main =
 
 -- MODEL
 type alias Model =
-  { textFilter: String
-  , filter: Maybe Int
-  , data: List (Maybe CompanyReport)
+  { data: List (Maybe CompanyReport)
   , statusText: Maybe String
   }
  
@@ -45,47 +43,31 @@ type ModelState
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  (Model  "" Nothing [] (Just "Specify the filter"), Cmd.none)
+  (Model [] (Just "Press the button"), Cmd.none)
 
 
 -- UPDATE
 type Msg 
-  = SaveSearchFilter String 
-  | StartSearch 
+  = StartSearch 
   | LoadReport ResponseModel
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    SaveSearchFilter filter ->
-      if filter == "" then 
-        ( { model | filter = Nothing, data = [], statusText = Just "Specify the filter" }, Cmd.none)
-      else 
-        case String.toInt filter of
-          Nothing ->
-            ( { model | filter = Nothing, data = [], statusText = Just "Incorrect filter (should be int)" }, Cmd.none)
-          Just someNumber ->
-            ( { model | filter = Just someNumber, statusText = Nothing }, Cmd.none)
-
     StartSearch ->
-      case model.filter of
-        Nothing ->
-          ( model, Cmd.none )
-        Just reportId ->
-          ( model, makeRequest )
+      ( model, makeRequest )
 
     LoadReport responseModel -> 
       case responseModel of
         RemoteData.Loading ->
-          ( { model | statusText = Just "Loading report..." }, Cmd.none)
+          ( { model | statusText = Just "Loading reports..." }, Cmd.none)
 
         RemoteData.Success data ->
           case data of
             Nothing ->
               ( { model | data = [], statusText = Just "Report not found" }, Cmd.none)
             Just reportList -> 
-              -- ( { model | data = Just [Just companyReport] , statusText = Just "Loaded some company report" }, Cmd.none)
-              ( { model | data = reportList,  statusText = Just "Loaded some company report" }, Cmd.none)
+              ( { model | data = reportList,  statusText = Just "Loaded some company reports" }, Cmd.none)
 
         RemoteData.NotAsked ->
           ( { model | statusText = Just "Starting load of the report" },  Cmd.none )
@@ -107,8 +89,7 @@ view : Model -> Html Msg
 view model =
   div []
     [ h2 [] [ text "Campaign report loader" ]
-    , input [ placeholder "Id of report", onInput SaveSearchFilter ] []
-    , searchButton model.filter
+    , button [ onClick StartSearch, disabled False ] [ text "Load all reports" ]
     , statusTextBar model.statusText
     , displayReportTable model
     ]
@@ -119,19 +100,9 @@ statusTextBar statusText =
     Nothing -> div [] [] 
     Just someString -> div [] [text someString] 
 
-searchButton : Maybe Int -> Html Msg
-searchButton filter = 
-  case filter of
-    Nothing ->
-      button [ onClick StartSearch, disabled True ] [ text "Search" ]
-    Just _ -> 
-      button [ onClick StartSearch, disabled False ] [ text "Search" ]
-    
-
 displayReportTable : Model -> Html Msg
 displayReportTable model =
   div [] (List.map displayReport model.data)
-
 
 displayReport : Maybe CompanyReport -> Html Msg
 displayReport report =
@@ -146,17 +117,17 @@ displayReport report =
 
 
 -- HTTP
-type alias MyResponse = Maybe CompanyReport
+type alias AllReportResponse = List (Maybe CompanyReport)
 
 type alias CompanyReport =
-  { companyName : Maybe String
+  { reportId : Scalar.Id
+  , companyName : Maybe String
   , companyBalance : Maybe Float
   }
 
 type alias ResponseModel =
-  RemoteData (Graphql.Http.Error (Maybe (List MyResponse))) (Maybe (List MyResponse))
+  RemoteData (Graphql.Http.Error (Maybe AllReportResponse)) (Maybe AllReportResponse)
 
--- , data: Maybe (List (Maybe CompanyReport))
 makeRequest : Cmd Msg
 makeRequest =
   reportQuery 
@@ -169,7 +140,8 @@ reportQuery =
 
 reportSelection : SelectionSet CompanyReport ReportApi.Object.CompanyReport
 reportSelection =
-  SelectionSet.map2 CompanyReport
+  SelectionSet.map3 CompanyReport
+    CompanyReport.reportId
     CompanyReport.companyName
     CompanyReport.companyBalance
 
